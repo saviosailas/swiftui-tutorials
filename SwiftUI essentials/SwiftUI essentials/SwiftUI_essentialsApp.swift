2e98059fab69f33292706f6532ac99bdbc3d32c3
savio <30656522+saviosailas@users.noreply.github.com>
2025-04-12 13:17:10 +0530

    created Landmarks project (swiftui essentials)

---
+++ SwiftUI essentials/SwiftUI essentials/SwiftUI_essentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_essentialsApp.swift
+//  SwiftUI essentials
+//
+//  Created by savio sailas on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_essentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
